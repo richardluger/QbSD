5a7dd3adfee01575537c007c5ad6123f41836bc4
Richard Luger <richard.luger@fsa.ulaval.ca>
2025-10-21 15:07:32 -0400

    .

---
+++ Tables_5-10 copy/Full_forecasting/08_run_TSX.R
@@ -0,0 +1,196 @@
+rm(list=ls())
+
+# Check if required packages are installed; install them if missing, then load
+packages <- c("quantreg", "Rcpp", "RcppDist")
+
+for (p in packages) {
+  if (!requireNamespace(p, quietly = TRUE)) {
+    install.packages(p)
+  }
+  library(p, character.only = TRUE)
+}
+
+
+# Load R and C++ functions
+source("../../Utilities/functions.R")       # R wrapper for QbSD_oos
+sourceCpp("../../Utilities/functions.cpp")  # C++ core computation
+
+
+####################################################################################################################################
+## Load the data and assign series names
+## 'Data.Rdata' contains returns for 8 international stock indices: S&P 500, DJIA, NASDAQ, EURO STOXX 50, FTSE 100, DAX, CAC 40, TSX
+####################################################################################################################################
+
+load("../../Data/Data.Rdata")
+
+bnam <- names(Data)
+nb <- length(bnam) 
+
+# Define start date for each series (in year + month/day format)
+bdate <- matrix(c(2002,250*10/12+18, # 10 months and 18 days into 2002
+                  2002,250*10/12+18,
+                  2002,250*10/12+18,
+                  2002,250*10/12+18,
+                  2002,250*10/12+18,
+                  2002,250*10/12+18,
+                  2002,250*10/12+18,
+                  2002,250*10/12+18),
+                nb, 2, byrow = TRUE)
+dimnames(bdate)[[1]] <- bnam       
+datt <- Data
+
+################################################################################################
+## Set global parameters for the forecasting experiment
+################################################################################################
+
+rw <- c(250, 1250, 2500)            	# rolling window sizes
+ALP <- c(0.01, 0.025, 0.05)         	# VaR and ES confidence levels
+
+# Settings for different methods
+RLOC <- c("zero", "QAR")           # location for robust QbSD_oos: constant vs. quantile autoregression
+gCAV  <- c("gSAV", "gAS")          # global CAViaR types: global symmetric absolute value, global asymmetric slope
+CAV  <- c("SAV", "AS")             # CAViaR types: symmetric absolute value, asymmetric slope
+RFO  <- c("VaRf", "ESf")           # forecasted VaR and ES
+LOC  <- c("zero", "AR")            # location for other models: constant vs. AR(1)
+ESD  <- c("mult", "ar")            # ES dynamics in AL_oos model
+gmo  <- c("GARCH_oos-n", "GARCH_oos-t", "GARCH_oos-skewt", "GJR-n", "GJR-t", "GJR-skewt", "EGARCH_oos-n")  # GARCH_oos variants
+
+# Initialize containers for forecast outputs 
+QbSD_oos <- list()
+AL_oos <- list()
+GAS_oos <- list()
+GARCH_oos  <- list()
+ 
+ 
+################################################################################################
+## Forecasting loop for one series (TSX)
+################################################################################################
+
+n <- 8  # index for TSX 
+ 
+dat <- as.numeric(datt[[n]])
+TT <- length(dat)
+
+# Initialize nested lists for each model 
+QbSD_oos[[n]] <- list()
+AL_oos[[n]] <- list()
+GAS_oos[[n]] <- list()
+GARCH_oos[[n]]  <- list()
+
+for (w in 1:length(rw)) {
+  
+  win <- rw[w]          # current rolling window size
+  fp <- TT - win        # number of out-of-sample forecasts
+  
+  QbSD_oos[[n]][[w]]  <- array(0,c(fp, 2, 2, 2, length(ALP)),dimnames = list(NULL, RFO, gCAV, RLOC, ALP))
+  AL_oos[[n]][[w]]    <- array(0,c(fp,2,2,2,2,length(ALP) ),dimnames=list(NULL,RFO,ESD,CAV,LOC,ALP ))
+  GAS_oos[[n]][[w]] <- array(0, c(fp, 2, 2, length(ALP)), dimnames = list(NULL, RFO, LOC, ALP))
+  GARCH_oos[[n]][[w]] <- array(0,c(fp,length(gmo),2,2,length(ALP) ),dimnames=list(NULL,gmo,RFO,LOC,ALP ))
+  
+  for (a in 1:length(ALP)) {
+    alpha <- ALP[a]  # VaR/ES probability level
+    
+    # Forecasts from QbSD method
+    
+    for (i in 1:fp) {
+      
+      print(paste(bnam[n],"-",rw[w],"-",alpha,"-",i))
+      
+      rets <- dat[i:(i + win - 1)]  # current rolling window
+      
+      ##########################################################################################
+      ## Quantile-based Scale Dynamics (QbSD_oos) forecasts
+      ##########################################################################################
+      
+      for (ir in 1:2) {
+        for (jr in 1:2) {
+          
+          results <- QbSD(rets, location=RLOC[ir], CAViaR=gCAV[jr], alpha=alpha )
+          
+          QbSD_oos[[n]][[w]][i,1,jr,ir,a] <- results$VaR_oos
+          QbSD_oos[[n]][[w]][i,2,jr,ir,a] <- results$ES_oos - results$mu_oos 			        			            
+        }
+      }
+      
+      ##########################################################################################
+      ## AL joint VaR and ES forecasts
+      ##########################################################################################
+      
+      for (itay in 1:2) {
+        for (jtay in 1:2) {
+          for (stay in 1:2)  {
+            
+            results <- AL(rets, location=LOC[itay], CAViaR=CAV[jtay], ES=ESD[stay], alpha=alpha)	  	              			                
+            AL_oos[[n]][[w]][i,,stay,jtay,itay,a] <- c(results$VaR_oos, results$ES_oos - results$mu_oos)
+          }
+        }
+      }
+      
+      ##########################################################################################
+      ## GAS forecasts
+      ##########################################################################################
+      
+      for (itay in 1:2) {
+        results <- GAS(rets, location = LOC[itay], alpha = alpha)
+        GAS_oos[[n]][[w]][i, , itay, a] <- c(results$VaR_oos, results$ES_oos - results$mu_oos)
+      }
+      
+      ##########################################################################################
+      ## GARCH family forecasts
+      ##########################################################################################
+      
+      for (ig in 1:2) {
+        
+        results <- GARCH_norm(rets, location=LOC[ig], alpha=alpha)
+        NormalVaRf <- results$VaR_oos
+        NormalESf  <- results$ES_oos - results$mu_oos 
+        
+        results <- GARCH_t(rets, location=LOC[ig], alpha=alpha)
+        StudentVaRf <- results$VaR_oos
+        StudentESf  <- results$ES_oos - results$mu_oos 
+        
+        results <- GARCH_skew_t(rets, location=LOC[ig], alpha=alpha)    
+        SkewtVaRf <- results$VaR_oos
+        SkewtESf  <- results$ES_oos - results$mu_oos 
+        
+        results <- GJR_norm(rets, location=LOC[ig], alpha=alpha)
+        GJRNormalVaRf <- results$VaR_oos
+        GJRNormalESf  <- results$ES_oos - results$mu_oos 
+        
+        results <- GJR_t(rets, location=LOC[ig], alpha=alpha)
+        GJRStudentVaRf <- results$VaR_oos
+        GJRStudentESf  <- results$ES_oos - results$mu_oos 
+        
+        results <- GJR_skew_t(rets, location=LOC[ig], alpha=alpha)
+        GJRskewtVaRf <- results$VaR_oos
+        GJRskewtESf  <- results$ES_oos - results$mu_oos 
+        
+        results <- EGARCH(rets, location=LOC[ig], alpha=alpha)
+        EGARCHNormalVaRf <- results$VaR_oos
+        EGARCHNormalESf  <- results$ES_oos - results$mu_oos 
+        
+        GARCH_oos[[n]][[w]][i,1,,ig,a] <- c(as.numeric(NormalVaRf),as.numeric(NormalESf))	
+        GARCH_oos[[n]][[w]][i,2,,ig,a] <- c(as.numeric(StudentVaRf),as.numeric(StudentESf) )
+        GARCH_oos[[n]][[w]][i,3,,ig,a] <- c(as.numeric(SkewtVaRf),as.numeric(SkewtESf) )
+        GARCH_oos[[n]][[w]][i,4,,ig,a] <- c(as.numeric(GJRNormalVaRf),as.numeric(GJRNormalESf))
+        GARCH_oos[[n]][[w]][i,5,,ig,a] <- c(as.numeric(GJRStudentVaRf),as.numeric(GJRStudentESf))
+        GARCH_oos[[n]][[w]][i,6,,ig,a] <- c(as.numeric(GJRskewtVaRf),as.numeric(GJRskewtESf))
+        GARCH_oos[[n]][[w]][i,7,,ig,a] <- c(as.numeric(EGARCHNormalVaRf),as.numeric(EGARCHNormalESf))
+        
+      }			
+    }
+  }
+}
+
+
+
+################################################################################################
+## Save forecast objects to RData files
+################################################################################################
+
+save(QbSD_oos,file="../Full_forecasting_output/QbSD.TSX.Rdata")
+save(AL_oos,file="../Full_forecasting_output/AL.TSX.Rdata")
+save(GAS_oos,file="../Full_forecasting_output/GAS.TSX.Rdata")
+save(GARCH_oos,file="../Full_forecasting_output/GARCH.TSX.Rdata")
+ 
+ 
---
+++ Tables_5-10 copy/Gen_tables/run_Tables_C7-C12.R
@@ -0,0 +1,410 @@
+rm(list = ls())  ## clear the workspace
+
+p <- "MCS"
+if (!requireNamespace(p, quietly = TRUE)) {
+  install.packages(p)
+}
+library(p, character.only = TRUE)
+
+set.seed(123456)
+
+load("../../Data/Data.Rdata")
+
+bnam <- names(Data)
+nb <- length(bnam) 
+
+datt <- list()
+for (n in 1:nb) {
+  datt[[n]] <- Data[[n]]
+}
+
+rw  <- c(250, 1250, 2500)
+ALP <- c(0.01, 0.025, 0.05)
+
+RLOC <- c("zero", "QAR")
+CAV  <- c("SAV", "AS")
+LOC  <- c("zero", "AR")
+ESD <- c("mult", "ar")
+gmo  <- c("GARCH-n", "GARCH-t", "GARCH-skewt", "GJR-n", "GJR-t", "GJR-skewt", "EGARCH-n")
+
+GARCHFF <- vector("list", nb)
+GASFF   <- vector("list", nb)
+QbSDFF  <- vector("list", nb)
+ALFF    <- vector("list", nb)
+
+####### load VaR and ES forecasts
+## GARCH
+load("../Full_forecasting_output/GARCH.SP500.Rdata");   GARCHFF[[1]] <- GARCH_oos[[1]]
+load("../Full_forecasting_output/GARCH.DJIA.Rdata");    GARCHFF[[2]] <- GARCH_oos[[2]]
+load("../Full_forecasting_output/GARCH.NASDAQ.Rdata");  GARCHFF[[3]] <- GARCH_oos[[3]]
+load("../Full_forecasting_output/GARCH.STOXX50.Rdata"); GARCHFF[[4]] <- GARCH_oos[[4]]
+load("../Full_forecasting_output/GARCH.FTSE.Rdata");    GARCHFF[[5]] <- GARCH_oos[[5]]
+load("../Full_forecasting_output/GARCH.DAX.Rdata");     GARCHFF[[6]] <- GARCH_oos[[6]]
+load("../Full_forecasting_output/GARCH.CAC40.Rdata");   GARCHFF[[7]] <- GARCH_oos[[7]]
+load("../Full_forecasting_output/GARCH.TSX.Rdata");     GARCHFF[[8]] <- GARCH_oos[[8]]
+
+## GAS
+load("../Full_forecasting_output/GAS.SP500.Rdata");     GASFF[[1]] <- GAS_oos[[1]]
+load("../Full_forecasting_output/GAS.DJIA.Rdata");      GASFF[[2]] <- GAS_oos[[2]]
+load("../Full_forecasting_output/GAS.NASDAQ.Rdata");    GASFF[[3]] <- GAS_oos[[3]]
+load("../Full_forecasting_output/GAS.STOXX50.Rdata");   GASFF[[4]] <- GAS_oos[[4]]
+load("../Full_forecasting_output/GAS.FTSE.Rdata");      GASFF[[5]] <- GAS_oos[[5]]
+load("../Full_forecasting_output/GAS.DAX.Rdata");       GASFF[[6]] <- GAS_oos[[6]]
+load("../Full_forecasting_output/GAS.CAC40.Rdata");     GASFF[[7]] <- GAS_oos[[7]]
+load("../Full_forecasting_output/GAS.TSX.Rdata");       GASFF[[8]] <- GAS_oos[[8]]
+
+## QbSD 
+load("../Full_forecasting_output/QbSD.SP500.Rdata");    QbSDFF[[1]] <- QbSD_oos[[1]]
+load("../Full_forecasting_output/QbSD.DJIA.Rdata");     QbSDFF[[2]] <- QbSD_oos[[2]]
+load("../Full_forecasting_output/QbSD.NASDAQ.Rdata");   QbSDFF[[3]] <- QbSD_oos[[3]]
+load("../Full_forecasting_output/QbSD.STOXX50.Rdata");  QbSDFF[[4]] <- QbSD_oos[[4]]
+load("../Full_forecasting_output/QbSD.FTSE.Rdata");     QbSDFF[[5]] <- QbSD_oos[[5]]
+load("../Full_forecasting_output/QbSD.DAX.Rdata");      QbSDFF[[6]] <- QbSD_oos[[6]]
+load("../Full_forecasting_output/QbSD.CAC40.Rdata");    QbSDFF[[7]] <- QbSD_oos[[7]]
+load("../Full_forecasting_output/QbSD.TSX.Rdata");      QbSDFF[[8]] <- QbSD_oos[[8]]
+
+## AL 
+load("../Full_forecasting_output/AL.SP500.Rdata");      ALFF[[1]] <- AL_oos[[1]]
+load("../Full_forecasting_output/AL.DJIA.Rdata");       ALFF[[2]] <- AL_oos[[2]]
+load("../Full_forecasting_output/AL.NASDAQ.Rdata");     ALFF[[3]] <- AL_oos[[3]]
+load("../Full_forecasting_output/AL.STOXX50.Rdata");    ALFF[[4]] <- AL_oos[[4]]
+load("../Full_forecasting_output/AL.FTSE.Rdata");       ALFF[[5]] <- AL_oos[[5]]
+load("../Full_forecasting_output/AL.DAX.Rdata");        ALFF[[6]] <- AL_oos[[6]]
+load("../Full_forecasting_output/AL.CAC40.Rdata");      ALFF[[7]] <- AL_oos[[7]]
+load("../Full_forecasting_output/AL.TSX.Rdata");        ALFF[[8]] <- AL_oos[[8]]
+
+
+GARCHF  <- GARCHFF
+GASF    <- GASFF
+QbSDF <- QbSDFF
+ALF     <- ALFF
+
+GARCHnam <- NULL
+for (s in 1:length(LOC)) {
+  for (g in 1:length(gmo)) {
+    GARCHnam <- c(GARCHnam, paste(gmo[g], LOC[s]))
+  }
+}
+GARCHmat <- matrix(1:(length(gmo) * length(LOC)), length(gmo), length(LOC))
+
+QbSDnam <- NULL
+for (s in 1:length(RLOC)) {
+  for (v in 1:length(CAV)) {
+    QbSDnam <- c(QbSDnam, paste("QbSD", RLOC[s], CAV[v]))
+  }
+}
+QbSDmat <- array(1:4, c(length(CAV), length(RLOC)))
+
+ALnam <- NULL
+for (s in 1:length(LOC)) {
+  for (v in 1:length(CAV)) {
+    for (d in 1:length(ESD)) {
+      ALnam <- c(ALnam, paste("AL", LOC[s], CAV[v], ESD[d]))
+    }
+  }
+}
+ALmat <- array(0, c(length(ESD), length(CAV), length(LOC)))
+ALmat[,,1] <- matrix(1:4,  length(ESD), length(CAV))
+ALmat[,,2] <- matrix(5:8,  length(ESD), length(CAV))
+
+QSS_GARCH  <- list()
+QSS_GAS <- list()
+QSS_QbSD <- list()
+QSS_AL <- list()
+
+ALLS_GARCH  <- list()
+ALLS_GAS <- list()
+ALLS_QbSD <- list()
+ALLS_AL <- list()
+
+
+
+# Build model name lists ONCE, before MCSS
+GASnam  <- paste("GAS", LOC)  
+Mnam    <- c(
+  (function(){
+    out <- character()
+    for (s in LOC) for (g in gmo) out <- c(out, paste(g, s))
+    out
+  })(),
+  GASnam,
+  (function(){
+    out <- character()
+    for (s in LOC) for (v in CAV) for (d in ESD) out <- c(out, paste("AL", s, v, d))
+    out
+  })(),
+  (function(){
+    out <- character()
+    for (s in RLOC) for (v in CAV) out <- c(out, paste("QbSD", s, v))
+    out
+  })()
+)
+nm   <- length(Mnam)
+Mnam1 <- Mnam  
+
+
+for (n in 1:nb) {
+
+  dat <- as.numeric(datt[[n]])
+  TT  <- length(dat)
+
+  QSS_GARCH[[n]]  <- list()
+  QSS_GAS[[n]] <- list()
+  QSS_QbSD[[n]] <- list()
+  QSS_AL[[n]] <- list()
+
+  ALLS_GARCH[[n]]  <- list()
+  ALLS_GAS[[n]] <- list()
+  ALLS_QbSD[[n]] <- list()
+  ALLS_AL[[n]] <- list()
+
+  for (w in 3) {  # <— run only w = 3  (= 2500)
+
+    win <- rw[w]
+    fp  <- TT - win
+    yf  <- dat[(win + 1):TT]
+
+    QSS_GARCH[[n]][[w]] <- array(0, c(fp, length(gmo) * length(LOC), length(ALP)), dimnames = list(NULL, GARCHnam, ALP))
+    QSS_GAS[[n]][[w]]  <- array(0, c(fp, length(LOC), length(ALP)), dimnames = list(NULL, LOC, ALP))
+    QSS_QbSD[[n]][[w]] <- list()
+    QSS_AL[[n]][[w]] <- array(0, c(fp, length(LOC) * length(ESD) * length(CAV), length(ALP)), dimnames = list(NULL, ALnam, ALP))
+
+    ALLS_GARCH[[n]][[w]] <- array(0, c(fp, length(gmo) * length(LOC), length(ALP)), dimnames = list(NULL, GARCHnam, ALP))
+    ALLS_GAS[[n]][[w]] <- array(0, c(fp, length(LOC), length(ALP)), dimnames = list(NULL, LOC, ALP))
+    ALLS_QbSD[[n]][[w]] <- list()
+    ALLS_AL[[n]][[w]] <- array(0, c(fp, length(LOC) * length(ESD) * length(CAV), length(ALP)), dimnames = list(NULL, ALnam, ALP))
+
+    for (a in 1:length(ALP)) {
+
+      print(c(bnam[n], rw[w], ALP[a]))
+      garch  <- GARCHF[[n]][[w]][,,,,a]
+      QbSD <- QbSDF[[n]][[w]][,,,, a] 
+      AL <- ALF[[n]][[w]][,,,,,a]
+
+      ## GARCH 
+      for (s in 1:length(LOC)) {
+        for (g in 1:length(gmo)) {
+          VAR    <- garch[, g, 1, s]
+          ES     <- garch[, g, 2, s]
+          qscore <- (yf - VAR) * (ALP[a] - (yf <= VAR) * 1)
+          QSS_GARCH[[n]][[w]][, GARCHmat[g, s], a]  <- qscore
+          ES <- pmin(ES, -1e-10)          
+          ALLS_GARCH[[n]][[w]][, GARCHmat[g, s], a] <- -log((ALP[a] - 1) / ES) - qscore / (ALP[a] * ES)
+        }
+      }
+
+      ## GAS 
+
+      GAS <- GASF[[n]][[w]][,,,a]   
+      for (s in 1:length(LOC)) {
+        VAR    <- GAS[, 1, s]
+        ES     <- GAS[, 2, s]
+        qscore <- (yf - VAR) * (ALP[a] - (yf <= VAR) * 1)
+        QSS_GAS[[n]][[w]][, s, a]  <- qscore
+        ES <- pmin(ES, -1e-10)          
+        ALLS_GAS[[n]][[w]][, s, a] <- -log((ALP[a] - 1) / ES) - qscore / (ALP[a] * ES)
+      }
+      
+      ## QbSD
+      QSS_QbSD[[n]][[w]][[a]] <- array(0, c(fp, length(CAV) * length(RLOC)), dimnames = list(NULL, QbSDnam) )
+      ALLS_QbSD[[n]][[w]][[a]] <- array(0, c(fp, length(CAV) * length(RLOC)), dimnames = list(NULL, QbSDnam))
+
+      for (s in 1:length(RLOC)) {
+        for (v in 1:length(CAV)) {
+          VAR    <- QbSD[, 1, v, s]
+          ES     <- QbSD[, 2, v, s]
+          dr     <- QbSDmat[v, s]
+          qscore <- (yf - VAR) * (ALP[a] - (yf <= VAR) * 1)
+          QSS_QbSD[[n]][[w]][[a]][, dr]  <- qscore
+          ES <- pmin(ES, -1e-10)          
+          ALLS_QbSD[[n]][[w]][[a]][, dr] <- -log((ALP[a] - 1) / ES) - qscore / (ALP[a] * ES)
+        }
+      }
+
+      ## AL
+      for (s in 1:length(LOC)) {
+        for (v in 1:length(CAV)) {
+          for (d in 1:length(ESD)) {
+            VAR    <- AL[, 1, d, v, s]
+            ES     <- AL[, 2, d, v, s]
+            dn     <- ALmat[d, v, s]
+            qscore <- (yf - VAR) * (ALP[a] - (yf <= VAR) * 1)
+            QSS_AL[[n]][[w]][, dn, a]  <- qscore
+            ES <- pmin(ES, -1e-10)          
+            ALLS_AL[[n]][[w]][, dn, a] <- -log((ALP[a] - 1) / ES) - qscore / (ALP[a] * ES)
+          }
+        }
+      }
+    }
+  }
+}
+
+########################################################################################
+########################################################################################
+########################################################################################
+########################### MCS
+
+MCSS_QS <- list()
+MCSS_AL <- list()
+
+for (n in 1:nb) {
+  MCSS_QS[[n]] <- list()
+  MCSS_AL[[n]] <- list()
+  for (w in 3) {  # <— run only w = 3
+    MCSS_QS[[n]][[w]] <- list()
+    MCSS_AL[[n]][[w]] <- list()
+
+    for (a in 1:length(ALP)) {
+      print(c(bnam[n], rw[w], ALP[a]))
+
+      QSS <- cbind(
+        QSS_GARCH[[n]][[w]][,,a],
+        QSS_GAS[[n]][[w]][, , a],       
+        QSS_AL[[n]][[w]][,,a],
+        QSS_QbSD[[n]][[w]][[a]]
+      )
+      colnames(QSS) <- Mnam
+      
+      ALLS <- cbind(
+        ALLS_GARCH[[n]][[w]][,,a],
+        ALLS_GAS[[n]][[w]][, , a],      
+        ALLS_AL[[n]][[w]][,,a],
+        ALLS_QbSD[[n]][[w]][[a]]
+      )
+      colnames(ALLS) <- Mnam
+      
+      k <- dim(QSS)[2]
+      MCSS_QS[[n]][[w]][[a]] <- MCSprocedure(Loss = QSS,  alpha = 0.1, statistic = "TR", B = 1000)
+      MCSS_AL[[n]][[w]][[a]] <- MCSprocedure(Loss = ALLS, alpha = 0.1, statistic = "TR", B = 1000)
+    }
+  }
+}
+
+############################################
+
+
+bnam <- c("SP500", "DJIA", "NASDAQ", "STOXX50", "FTSE", "DAX", "CAC40", "TSX")
+
+nb  <- length(bnam)
+rw  <- c(250, 1250, 2500)
+ALP <- c(0.01, 0.025, 0.05)
+
+########### Produce Tables C10,C11,C12
+
+MCSR_AL <- array( 0, c(length(Mnam1), nb, length(ALP), length(rw)), dimnames = list(Mnam1, bnam, ALP, rw))
+
+for (n in 1:nb) {
+  for (w in 3) {  # <— run only w = 3
+    for (a in 1:length(ALP)) {
+      mc   <- MCSS_AL[[n]][[w]][[a]]@show
+      snam <- dimnames(mc)[[1]]
+      for (i in 1:length(snam)) {
+        loc <- which(snam[i] == Mnam)
+        MCSR_AL[loc, n, a, w] <- mc[i, 4]
+      }
+    }
+  }
+}
+
+MCS_AL_Rank <- list()
+w <- 3
+for (a in 1:length(ALP)) {
+  mm <- MCSR_AL[,,a,w]
+  MA <- matrix(
+    0, nrow = dim(mm)[1], ncol = dim(mm)[2] + 3,
+    dimnames = list(Mnam1, c(bnam, "#", "Avg.rank", "Final.rank"))
+  )
+
+  MA[, 1:nb] <- mm
+  for (i in 1:nm) {
+    MA[i, nb + 1] <- sum(mm[i, ] != 0)
+    ind           <- (mm[i, ] == 0) * 1
+    MA[i, nb + 2] <- round((sum(ind) * nm + sum((1 - ind) * mm[i, ])) / nb, 1)
+  }
+
+  od <- order(MA[, nb + 2])
+  MCS_AL_Rank[[a]] <- MA[od, ]
+  MCS_AL_Rank[[a]][, nb + 3] <- 1:nm
+}
+
+TableC10  <- MCS_AL_Rank[[1]]
+TableC11  <- MCS_AL_Rank[[2]]
+TableC12  <- MCS_AL_Rank[[3]]
+for (i in 1:nm) {
+  for (j in 1:nb) {
+    if (TableC10[i, j]  == 0) TableC10[i, j]  <- NA  ## "NA" represents empty cells
+    if (TableC11[i, j]  == 0) TableC11[i, j]  <- NA
+    if (TableC12[i, j] == 0)  TableC12[i, j]  <- NA
+  }
+}
+
+print("Table C10");  TableC10
+print("Table C11");  TableC11
+print("Table C12");  TableC12
+
+write.table(TableC10, file = "TableC10.txt", sep = "\t", quote = FALSE, na = "", col.names = NA)
+write.table(TableC11, file = "TableC11.txt", sep = "\t", quote = FALSE, na = "", col.names = NA)
+write.table(TableC12, file = "TableC12.txt", sep = "\t", quote = FALSE, na = "", col.names = NA)
+
+
+
+########### Produce Tables C7,C8,C9
+
+MCSR_QS <- array(
+  0, c(length(Mnam1), nb, length(ALP), length(rw)),
+  dimnames = list(Mnam1, bnam, ALP, rw)
+)
+
+for (n in 1:nb) {
+  for (w in 3) {  # <— run only w = 3
+    for (a in 1:length(ALP)) {
+      mc   <- MCSS_QS[[n]][[w]][[a]]@show
+      snam <- dimnames(mc)[[1]]
+      for (i in 1:length(snam)) {
+        loc <- which(snam[i] == Mnam)
+        MCSR_QS[loc, n, a, w] <- mc[i, 4]
+      }
+    }
+  }
+}
+
+MCS_QS_Rank <- list()
+w <- 3
+for (a in 1:length(ALP)) {
+  mm <- MCSR_QS[,,a,w]
+  MA <- matrix(
+    0, nrow = dim(mm)[1], ncol = dim(mm)[2] + 3,
+    dimnames = list(Mnam1, c(bnam, "#", "Avg.rank", "Final.rank"))
+  )
+
+  MA[, 1:nb] <- mm
+  for (i in 1:nm) {
+    MA[i, nb + 1] <- sum(mm[i, ] != 0)
+    ind           <- (mm[i, ] == 0) * 1
+    MA[i, nb + 2] <- round((sum(ind) * nm + sum((1 - ind) * mm[i, ])) / nb, 1)
+  }
+
+  od <- order(MA[, nb + 2])
+  MCS_QS_Rank[[a]] <- MA[od, ]
+  MCS_QS_Rank[[a]][, nb + 3] <- 1:nm
+}
+
+TableC7 <- MCS_QS_Rank[[1]]
+TableC8 <- MCS_QS_Rank[[2]]
+TableC9 <- MCS_QS_Rank[[3]]
+for (i in 1:nm) {
+  for (j in 1:nb) {
+    if (TableC7[i, j] == 0) TableC7[i, j] <- NA  ## "NA" represents empty cells
+    if (TableC8[i, j] == 0) TableC8[i, j] <- NA
+    if (TableC9[i, j] == 0) TableC9[i, j] <- NA
+  }
+}
+
+print("Table C7"); TableC7
+print("Table C8"); TableC8
+print("Table C9"); TableC9
+
+write.table(TableC7, file = "TableC7.txt", sep = "\t", quote = FALSE, na = "", col.names = NA)
+write.table(TableC8, file = "TableC8.txt", sep = "\t", quote = FALSE, na = "", col.names = NA)
+write.table(TableC9, file = "TableC9.txt", sep = "\t", quote = FALSE, na = "", col.names = NA)
+
